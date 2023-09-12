6bdbf68cfaa69ce3cc37804c98ac941d44bf0546
Pranay Ramteke <pranayramteke613@gmail.com>
2023-09-12 07:33:04 -0700

    Added new code files

--- Main_72_List.dart
+++ Main_72_List.dart
@@ -3,6 +3,9 @@ void main(List<String> args) {
   // [10,20,30]
   // Dynamic List
   List list_instance = [10, 20, 30];
+  List<double> listDouble = [10, 10, 12.12];
+
+  print(listDouble);
   print(list_instance[0]);
   print(list_instance[2]);
 
--- Main_73_List.dart
+++ Main_73_List.dart
@@ -5,7 +5,16 @@ class Student<T> {
   Student(this.name);
 }
 
+class Person {
+  String? name;
+}
+
 void main(List<String> args) {
   Student<String> s = new Student<String>("Raj");
+  List<Person> personList = List.empty(growable: true);
+  personList.add(new Person());
+  personList.add(new Person());
+  print(personList);
+
   print(s.name);
 }
