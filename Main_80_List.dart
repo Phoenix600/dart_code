0f56b27b825dea20255290d486f675ab8b867a57
Pranay Ramteke <pranayramteke613@gmail.com>
2023-09-15 23:42:55 -0700

    Added new code files

--- Main_80_List.dart
+++ Main_80_List.dart
@@ -1,1 +1,25 @@
-void main(List<String> args) {}
+class Student {
+  final String name;
+  final int roll;
+  Student(this.name, this.roll);
+
+  String toString() => "${this.name}.....${this.roll}";
+}
+
+void main(List<String> args) {
+  // Getting Started With
+  List<Student> studentList = [];
+
+  Student someStudent = new Student("Pranay Ramteke", 20001);
+  studentList.add(new Student("Pranay G", 12));
+  studentList.add(someStudent);
+  studentList.insert(0, new Student("Mithilesh Mothgare", 69));
+  studentList.insert(0, new Student("Parth Pathak", 1212));
+  studentList.add(new Student("Harshal Narnaware", 31));
+
+  print(studentList);
+
+  studentList.remove(someStudent);
+
+  print(studentList);
+}
